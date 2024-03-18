40a66d87d7008cbcb8d02a7c6670739b665098ed
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2024-03-18 18:53:38 +0100

    SUBIR PRACTICA ACABABA

--- Ex2/VersionPHP_JS/app.js
+++ Ex2/VersionPHP_JS/app.js
@@ -1,62 +1,61 @@
-let btnEdit = document.querySelectorAll(".btnEdit");
-btnEdit.forEach(el => {
-    el.addEventListener("click", function () {
+const editButtons = document.querySelectorAll('.btnEdit');
+const deleteButtons = document.querySelectorAll('.btnDelete');
 
-        let formData = new FormData();
-        formData.append("id", this.getAttribute("idProd"));
+editButtons.forEach(button => button.addEventListener("click", editProduct));
+deleteButtons.forEach(button => button.addEventListener("click", deleteProduct));
 
-        let options = {
-            method: 'POST',
-            body: formData
-        }
+function createFetchOptions(post, formData) {
+    return {
+        method: post,
+        body: formData
+    }
+}
 
-        fetch("getProducte.php", options)
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                document.getElementById("productName").value = data.name;
-                document.getElementById("productId").value = data.id;
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
+function productData(data) {
+    document.getElementById("productName").value = data.name;
+    document.getElementById("productId").value = data.id;
+}
 
-    })
-})
-let btnDelete = document.querySelectorAll(".btnDelete");
-btnDelete.forEach(el => {
-    el.addEventListener("click", function () {
+function fetchProductData(php, options, dto) {
+    fetch(php, options)
+        .then(response => response.json())
+        .then(data => dto(data)
+            .catch(error => console.error(error)));
+}
 
-        let formData = new FormData();
-        formData.append("id", this.getAttribute("idProd"));
+function editProduct() {
+    const productId = this.getAttribute("idProd");
+    const formData = createFormData("id", productId);
+    const options = createFetchOptions('POST', formData);
+    fetchProductData("getProducte.php", options, productData);
+}
 
-        let options = {
-            method: 'POST',
-            body: formData
-        }
+function deleteProduct() {
+    const productId = this.getAttribute("idProd");
+    const formData = createFormData("id", productId);
+    const options = createFetchOptions('POST', formData);
+    fetchProductData("deleteProduct.php", options, handleProductDeletion);
 
-        fetch("deleteProduct.php", options)
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                showAlert(data.message, data.status)
-                setTimeout(() => {
-                    location.reload();
-                }, 1500);
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    })
-})
+}
+
+function createFormData(id, productId) {
+    const formData = new FormData();
+    formData.append(id, productId);
+    return formData;
+}
+
+function handleProductDeletion(data) {
+    showAlert(data.message, data.status);
+    setTimeout(() => location.reload(), 1500);
+}
 
 function showAlert(message, type) {
     const alertPlaceholder = document.getElementById('message');
     const alertBox = `
-            <div class="alert alert-${type} alert-dismissible fade show" role="alert">
-                ${message}
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-        `;
+        <div class="alert alert-${type} alert-dismissible fade show" role="alert">
+            ${message}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    `;
     alertPlaceholder.innerHTML = alertBox;
 }
